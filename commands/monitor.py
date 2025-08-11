8d72d73b7c2e2fbf91b0e3ddad3041d49a071442
Tirth9000 <sharmatirth09012@gmail.com>
2025-08-11 21:42:45 +0530

    Add initial backend structure with FastAPI and Docker monitoring commands

--- commands/monitor.py
+++ commands/monitor.py
@@ -1,11 +1,22 @@
-import typer
+from rich.live import Live
+from rich.console import Console
+from .operations import get_table
+
+
+console = Console()
+
 
 
 def init():
     print("Initializing monitor commands...")
 
-def monitor():
-    print("Monitoring...")
+
+def monitor(refresh: int = 1):
+     with Live(get_table(), refresh_per_second=3, console=console) as live:
+        while True:
+            live.update(get_table())
+
+
 
 def status():
     print("Checking status...")
